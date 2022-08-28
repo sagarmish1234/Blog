60cad4bf7f39c774c3e38c589e51b115344d2a06
unknown <sagarmish1234@gmail.com>
2022-08-28 11:42:43 +0530

    User repos added along with jjwt and jaxbi

--- blog-microservices-services/auth-service/src/main/java/com/blogserver/authservice/AuthServiceApplication.java
+++ blog-microservices-services/auth-service/src/main/java/com/blogserver/authservice/AuthServiceApplication.java
@@ -2,11 +2,12 @@ package com.blogserver.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
+@EnableWebSecurity
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
---
+++ blog-microservices-services/auth-service/src/main/java/com/blogserver/authservice/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.blogserver.authservice.repository;
+
+import com.blogserver.authservice.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findByEmail(String email);
+
+    Optional<List<UserEntity>> findByFullNameIgnoreCaseContaining(String name);
+}
