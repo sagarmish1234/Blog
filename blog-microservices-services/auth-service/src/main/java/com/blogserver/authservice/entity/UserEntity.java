182d9e592e58216ee81c6da4563d88d71da6e9ff
unknown <sagarmish1234@gmail.com>
2022-08-28 11:26:18 +0530

    Added user entity

---
+++ blog-microservices-services/auth-service/src/main/java/com/blogserver/authservice/entity/UserEntity.java
@@ -0,0 +1,30 @@
+package com.blogserver.authservice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String image;
+    private String fullName;
+    private String email;
+    private String password;
+    private String phone;
+    private Date createdAt;
+    private String role;
+}
