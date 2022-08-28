77eede968d156d6f5ea279899576f8d5e0ddd6cd
unknown <sagarmish1234@gmail.com>
2022-08-28 12:05:07 +0530

    Added user service

--- blog-microservices-services/auth-service/src/main/java/com/blogserver/authservice/AuthServiceApplication.java
+++ blog-microservices-services/auth-service/src/main/java/com/blogserver/authservice/AuthServiceApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableWebSecurity
+@EnableTransactionManagement
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
---
+++ blog-microservices-services/auth-service/src/main/java/com/blogserver/authservice/service/UserService.java
@@ -0,0 +1,91 @@
+package com.blogserver.authservice.service;
+
+
+import com.blogserver.authservice.entity.UserEntity;
+import com.blogserver.authservice.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<UserEntity> user = userRepository.findByEmail(username);
+        if(user.isPresent()){
+            return new User(user.get().getEmail(),user.get().getPassword(),new ArrayList<>());
+        }
+        throw new UsernameNotFoundException("User not found");
+    }
+
+    public UserEntity loadUserById(Long id) throws UsernameNotFoundException{
+        Optional<UserEntity> user = userRepository.findById(id);
+        if(user.isPresent()){
+            return user.get();
+        }
+        throw new UsernameNotFoundException("User not found");
+    }
+
+    public Optional<UserEntity> loadUserByEmail(String email){
+        return userRepository.findByEmail(email);
+    }
+
+    public List<UserEntity> loadUsersByName(String name) throws Exception{
+        Optional<List<UserEntity>> users = userRepository.findByFullNameIgnoreCaseContaining(name);
+        if(users.isPresent() && !(users.get().isEmpty())){
+            return users.get();
+        }
+        throw new Exception("No users with the given name found");
+    }
+
+    public UserEntity saveUser(UserEntity user) {
+        user.setCreatedAt(new Date());
+        user.setRole("User");
+        return userRepository.save(user);
+    }
+
+    public void deleteUserById(Long id){
+        userRepository.deleteById(id);
+    }
+
+    public UserEntity updateUserById(Long id, UserEntity newUserEntity) throws Exception{
+        UserEntity userEntity = this.loadUserById(id);
+        userEntity.setFullName(newUserEntity.getFullName());
+        userEntity.setPassword(newUserEntity.getPassword());
+        userEntity.setPhone(newUserEntity.getPhone());
+        userEntity.setEmail(newUserEntity.getEmail());
+        userEntity.setRole(newUserEntity.getRole());
+        userEntity.setImage(newUserEntity.getImage());
+        return userEntity;
+    }
+
+    public UserEntity updateUserByUsername(String username,UserEntity newUserEntity) throws Exception{
+        Optional<UserEntity> userEntity = userRepository.findByEmail(username);
+        if(userEntity.isPresent()){
+            userEntity.get().setFullName(newUserEntity.getFullName());
+            userEntity.get().setPassword(new BCryptPasswordEncoder().encode(newUserEntity.getPassword()));
+            userEntity.get().setPhone(newUserEntity.getPhone());
+            userEntity.get().setEmail(newUserEntity.getEmail());
+            userEntity.get().setRole(newUserEntity.getRole());
+            return userEntity.get();
+        }
+        throw new Exception("No user found");
+
+    }
+
+}
