5ffc1d0fe345466da5831a574cd1eba006498fb5
unknown <sagarmish1234@gmail.com>
2022-08-28 11:20:08 +0530

    Auth service added

---
+++ blog-microservices-services/auth-service/src/main/java/com/blogserver/authservice/AuthServiceApplication.java
@@ -0,0 +1,16 @@
+package com.blogserver.authservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class AuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServiceApplication.class, args);
+	}
+
+}
