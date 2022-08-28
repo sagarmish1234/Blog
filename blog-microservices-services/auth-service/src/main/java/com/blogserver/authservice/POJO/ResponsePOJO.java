4bd699d8d9f4b27b97b625881bfa4ea7a53b992f
unknown <sagarmish1234@gmail.com>
2022-08-28 12:33:13 +0530

    Added pojos

---
+++ blog-microservices-services/auth-service/src/main/java/com/blogserver/authservice/POJO/ResponsePOJO.java
@@ -0,0 +1,14 @@
+package com.blogserver.authservice.POJO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponsePOJO {
+
+    private String message;
+
+}
